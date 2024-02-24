f98203a9825ea2b9b6f9cfa7abfdd95fff560b9c
Edmund Lim <elimcw20@gmail.com>
2024-02-24 18:14:15 +0800

    Update images-cards
    change of display type to gameboard, inserted images and flip card animation(still testing and WIP)

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 //grab list of card divs
 const cardslist = document.querySelectorAll(".cards");
 
+//Root card flip variables
+let cardIsFlipped = false;
+let card1 = undefined;
+let card2 = undefined;
+
 //Add event listener to each card divs
-//function to check if target is being clicked
+
 function flipCard() {
-  console.log("clicked");
-  console.log(this);
+  this.classList.add("flip");
+  if (cardIsFlipped !== true) {
+    cardIsFlipped = true;
+    card1 = this;
+    console.log(cardIsFlipped, card1);
+  } else {
+    cardIsFlipped = false;
+    card2 = this;
+    console.log(card1, card2);
+  }
 }
 cardslist.forEach((card) => card.addEventListener("click", flipCard));
